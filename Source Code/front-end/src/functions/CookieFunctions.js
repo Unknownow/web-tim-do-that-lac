932f272a4a3732a75f665607ae4fd21535118a2d
datngusi <thainga0909@gmail.com>
2019-12-08 23:21:28 +0700

    add encrypt token in cookie

--- Source Code/front-end/src/functions/CookieFunctions.js
+++ Source Code/front-end/src/functions/CookieFunctions.js
@@ -8,6 +8,16 @@ export const CookieFunctions = {
     } else {
       expires = "";
     }
+
+    value = btoa(value);
+    for (let index = 0; index < value.length; index++) {
+      if (index % 2 === 1) {
+        let x = Math.random()
+          .toString(36)
+          .substring(4, 5);
+        value = value.slice(0, index) + x + value.slice(index);
+      }
+    }
     document.cookie = name + "=" + value + expires + "; path=/";
   },
   readCookie: function(name) {
@@ -22,7 +32,22 @@ export const CookieFunctions = {
         c = c.substring(1, c.length);
       }
       if (c.indexOf(nameEQ) == 0) {
-        return c.substring(nameEQ.length, c.length);
+        let value = c.substring(nameEQ.length, c.length); // read from cookie
+
+        for (let index = 0; index < value.length; index++) {
+          if (index % 2 === 1 && value.length % 2 === 1) {
+            let part1 = value.substring(0, index);
+            let part2 = value.substring(index + 1, value.length);
+            value = part1 + part2;
+          }
+          if (index % 2 === 0 && value.length % 2 === 0) {
+            let part1 = value.substring(0, index);
+            let part2 = value.substring(index + 1, value.length);
+            value = part1 + part2;
+          }
+        }
+        value = atob(value);
+        return value;
       }
     }
     return "";
--- Source Code/front-end/testxxx.js
+++ Source Code/front-end/testxxx.js
@@ -5,6 +5,30 @@
 // var date = new Date(decoded.iat);
 // console.log(date);
 
-var abc = "12321321321";
-var abc1 = abc.slice(0 , abc.length - 4);
-console.log(abc1);
+// var abc = "12321321321";
+// var abc1 = abc.slice(0 , abc.length - 4);
+// console.log(abc1);
+
+var value = "123456789";
+for (let index = 0; index < value.length; index++) {
+  if (index % 2 === 1) {
+    let x = Math.random()
+      .toString(36)
+      .substring(4, 5);
+    value = value.slice(0, index) + x + value.slice(index);
+  }
+}
+console.log(value);
+for (let index = 0; index < value.length; index++) {
+  if (index % 2 === 1 && value.length % 2 === 1) {
+    part1 = value.substring(0, index);
+    part2 = value.substring(index + 1, value.length);
+    value = part1 + part2;
+  }
+  if (index % 2 === 0 && value.length % 2 === 0) {
+    part1 = value.substring(0, index);
+    part2 = value.substring(index + 1, value.length);
+    value = part1 + part2;
+  }
+}
+console.log(value);
